87bde204e432a1e4ffb71b30f3ca96572933f667
MushPvp <86511465+MushPvp@users.noreply.github.com>
2021-06-26 17:40:46 +0200

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -4,11 +4,8 @@
   <h1 align="center">Nous verifions que tu est humain...</h1>
   <p align="center">Le processus anti attaques DDos dure au maximum 5 secondes</p>
   <img align="center" src="https://www.stargayzers.org/images/loading193x192.gif">
-  <script> 
-function end(){
-document.location.href="https://mushpvp.ml/antiddos.php"
-}
-setTimeout(end(), 5000)
-  </script>
  </head>
 </html>
+<?php
+sleep(5);
+echo "<html><head><script>document.location.href='antiddos.php'</script></head></html>";
