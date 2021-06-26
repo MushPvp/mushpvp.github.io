5134841a483665ca18ca12df62eb72b7b45a5037
MushPvp <86511465+MushPvp@users.noreply.github.com>
2021-06-26 10:05:29 +0200

    Update mobile.js

--- js/mobile.js
+++ js/mobile.js
@@ -1,3 +1,8 @@
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
-    document.write("Salut")
+
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+  // true for mobile device
+  document.write("mobile device");
+}else{
+  // false for not mobile device
+  document.write("not mobile device");
 }
