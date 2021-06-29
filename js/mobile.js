14a0b78e015326b820b7009664e3cbee52906a44
MushPvp <86511465+MushPvp@users.noreply.github.com>
2021-06-29 18:00:13 +0200

    Delete js directory

--- js/mobile.js
+++
@@ -1,8 +0,0 @@
-
-if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-  // true for mobile device
-  document.write("mobile device");
-}else{
-  // false for not mobile device
-  document.write("not mobile device");
-}
